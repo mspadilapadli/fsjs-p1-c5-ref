c7add36bd410bda1e9172b4ed25a2c894c1bf471
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2026-01-06 21:53:14 +0700

    add newColoum to model init

---
+++ models/art.js
@@ -0,0 +1,37 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+    class Art extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+
+        static async getArts() {
+            try {
+                return await Art.findAll();
+            } catch (error) {
+                throw error;
+            }
+        }
+    }
+    Art.init(
+        {
+            name: DataTypes.STRING,
+            artist: DataTypes.STRING,
+            date: DataTypes.DATE,
+            description: DataTypes.TEXT,
+            photo: DataTypes.STRING,
+            placeOfOrigin: DataTypes.STRING,
+        },
+        {
+            sequelize,
+            modelName: "Art",
+        }
+    );
+    return Art;
+};
