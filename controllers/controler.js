ced13df4deb45319152c3d09a007de88d180486d
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2026-01-07 19:10:55 +0700

    R1: getRead()

--- controllers/controler.js
+++ controllers/controler.js
@@ -3,15 +3,7 @@ class Controller {
     static async readArts(req, res) {
         try {
             const data = await Art.findAll();
-            res.send(data);
-        } catch (error) {
-            console.log(error.message);
-        }
-    }
-    static async readArts(req, res) {
-        try {
-            const data = await Art.getArts();
-            res.send(data);
+            res.render("arts", { data });
         } catch (error) {
             console.log(error.message);
         }
--- routers/index.js
+++ routers/index.js
@@ -3,5 +3,11 @@ const router = express.Router();
 const Controller = require("../controllers/controler");
 
 router.get("/", Controller.readArts);
+// router.get("/arts/add");
+// router.post("/arts/add");
+// router.get("/arts/edit/:id");
+// router.post("/arts/edit/:id");
+// router.get("/arts/delete/:id");
+// router.get("/arts/:id");
 
 module.exports = router;
