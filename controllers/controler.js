65b79166334ef415891b03f029d93496e5d84bf7
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2026-01-07 19:59:30 +0700

    R1: postAdd()

--- controllers/controler.js
+++ controllers/controler.js
@@ -8,6 +8,42 @@ class Controller {
             console.log(error.message);
         }
     }
+
+    static async getFormAddoUpdate(req, res) {
+        try {
+            const { id } = req.params;
+            let art = {};
+            let action = "/arts/add";
+
+            if (id) {
+                art = await Art.findByPk(id);
+                action = `/arts/edit/${id}`;
+            }
+            res.render("showForm", { art, action, error: {}, isEdit: false });
+        } catch (error) {
+            console.log(error);
+            // res.send(error);
+        }
+    }
+
+    static async postAdd(req, res) {
+        try {
+            let { name, artist, date, photo, placeOfOrigin, description } =
+                req.body;
+            let payload = {
+                name,
+                artist,
+                date,
+                photo,
+                placeOfOrigin,
+                description,
+            };
+            await Art.create(payload);
+            res.redirect("/");
+        } catch (error) {
+            res.send(error);
+        }
+    }
 }
 
 module.exports = Controller;
--- routers/index.js
+++ routers/index.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const Controller = require("../controllers/controler");
 
 router.get("/", Controller.readArts);
-// router.get("/arts/add");
-// router.post("/arts/add");
-// router.get("/arts/edit/:id");
+router.get("/arts/add", Controller.getFormAddoUpdate);
+router.post("/arts/add", Controller.postAdd);
+// router.get("/arts/edit/:id", Controller.getFormAddoUpdate);
 // router.post("/arts/edit/:id");
 // router.get("/arts/delete/:id");
 // router.get("/arts/:id");
